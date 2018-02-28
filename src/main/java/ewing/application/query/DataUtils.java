3d87fce8854b494fb9f9b1864a5eff988e3b9fd2
EwingTsai <191237878@qq.com>
2018-02-28 11:48:56 +0800

    更新项目文件

--- src/main/java/ewing/application/query/DataUtils.java
+++ src/main/java/ewing/application/query/DataUtils.java
@@ -9,6 +9,9 @@ public class DataUtils {
     private DataUtils() {
     }
 
+    /**
+     * 获取最多只有一个元素的集合中的元素。
+     */
     public static <E, C extends Collection<E>> E getMaxOne(C collection) {
         if (collection == null || collection.size() < 1) {
             return null;
@@ -19,11 +22,17 @@ public class DataUtils {
         }
     }
 
+    /**
+     * 复制对象属性并返回目标对象。
+     */
     public static <T> T copyProperties(Object source, T target) {
         BeanUtils.copyProperties(source, target);
         return target;
     }
 
+    /**
+     * 获取最多只有一个元素的集合中的元素，复制该元素的属性并返回目标对象。
+     */
     public static <E, C extends Collection<E>, T> T getMaxOneAndCopy(C source, T target) {
         E one = getMaxOne(source);
         if (one == null || target == null) {
