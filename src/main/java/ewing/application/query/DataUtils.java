5a15f61234266c46a62578ecaaa60bd489a661ac
EwingTsai <191237878@qq.com>
2018-02-28 17:03:07 +0800

    添加获取第一个

--- src/main/java/ewing/application/query/DataUtils.java
+++ src/main/java/ewing/application/query/DataUtils.java
@@ -23,6 +23,17 @@ public class DataUtils {
     }
 
     /**
+     * 获取最多只有一个元素的集合中的元素。
+     */
+    public static <E, C extends Collection<E>> E getFirstOne(C collection) {
+        if (collection == null || collection.size() < 1) {
+            return null;
+        } else {
+            return collection.iterator().next();
+        }
+    }
+
+    /**
      * 复制对象属性并返回目标对象。
      */
     public static <T> T copyProperties(Object source, T target) {
